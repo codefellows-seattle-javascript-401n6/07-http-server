133f5e0aba0413d1aaebf85f5aef11e73b231984
FREDRIC THOMAS RAPP <fred.rapp@gmail.com>
2018-02-22 21:15:24 -0800

    Added code to index.html

--- lab07-fredric-401n/index.js
+++ lab07-fredric-401n/index.js
@@ -1,1 +1,9 @@
 "use strict";
+
+require('dotenv').config();
+
+const server = require('./lib/server');
+
+server.start(process.env.PORT, () => {
+  console.log(`Your server is running on PORT ${process.env.PORT}`);
+});
