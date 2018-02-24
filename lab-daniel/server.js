be00f1f62636cc67de8c6088de8988479f67d694
daniel <dpillay03@gmail.com>
2018-02-24 11:44:13 -0800

    server is  sending baack html with a project description and a anchor to /cowsay.

--- lab-daniel/server.js
+++ lab-daniel/server.js
@@ -7,11 +7,33 @@ const PORT = process.ENV || 3000;
 const server = http.createServer((req, res) => {
     req.url = url.parse(req.url);
     req.url.query = querystring.parse(req.url.query);
-    console.log('url: ', req.url)
+    console.log('url: ', req.url);
+
+    req.on('data', (buffer) => {
+        console.log('Buffer: ', buffer.toString());
+    })
+    res.write(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title> cowsay </title>  
+      </head>
+      <body>
+       <header>
+         <nav>
+           <ul> 
+             <li><a href="/cowsay">cowsay</a></li>
+           </ul>
+         </nav>
+       <header>
+       <main>
+         <!-- project description -->
+       </main>
+      </body>
+    </html>`);
+    
+    res.end();
 })
-
-
-
 
 
 server.listen(PORT, () => {
