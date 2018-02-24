f7277e6e81bdf2ee6ddc6e1dd603cd7163469955
daniel <dpillay03@gmail.com>
2018-02-24 11:19:40 -0800

    created server file. Port is running.

--- lab-daniel/server.js
+++ lab-daniel/server.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const PORT = process.ENV || 3000;
+const server = http.createServer((req, res) => {
+    req.url = url.parse(req.url);
+    req.url.query = querystring.parse(req.url.query);
+    console.log('url: ', req.url)
+})
+
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`Your port is on: ${PORT}`);
+})
