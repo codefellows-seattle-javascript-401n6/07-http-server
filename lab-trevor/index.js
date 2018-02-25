e376d644f0835cefae34c615153be751b4551d66
trevorjdobson <trevorjdobson@gmail.com>
2018-02-25 11:39:49 -0800

    working on get and post

--- lab-trevor/index.js
+++ lab-trevor/index.js
@@ -3,22 +3,39 @@
 const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
+const cowsay = require('cowsay');
+
+const  PORT = process.env.PORT || 3000;
+
+function bodyParser(req, cb){
+    if(req.method === 'POST'){
+        let body = '';
+        req.on('data', (buf) => {
+            body += buf.toString();
+        })
+        req.on('end', () => cb(null, body));
+        req.on('error', (err) => cb(err));
+    } else {
+        cb(null, '{}');
+    }
+}
 
 const server = http.createServer((req, res) => {
     req.url = url.parse(req.url);
     req.url.query =  querystring.parse(req.url.query);
     console.log('url', req.url);
 
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-        //'Content-Type': 'application/json'
-        //'Content-Type': text/html; charset=utf-8
-    });
-
-    res.write("hellllo");
+    if(req.url.pathname === '/cowsay'){
+        if(req.method === 'GET'){
+            res.write('uhhh')
+        }
+        if(req.method === 'POST'){
+            res.write('helllllp')
+        }
+    }
 })
 
-const  PORT = process.ENV || 3000;
+
 server.listen(PORT, () => {
     console.log('listening on port' + PORT);
 });
