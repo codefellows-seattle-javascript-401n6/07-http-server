4c01f2615d01d437c52e21a396bd7af1185f416c
rjtj2007 <teacherman1@gmail.com>
2018-02-25 22:06:33 -0800

    added the html form  to the dependencies and body parcer

--- lab-ryan/server.js
+++ lab-ryan/server.js
@@ -7,17 +7,59 @@ const cowsay = require('cowsay');
 
 const PORT = process.env.PORT || 3000;
 
+// callback should be (err, body) => undefined
+const bodyParse = (req, callback) => {
+    if (req.method === 'POST' || req.method === 'PUT') {
+        let body = ''
+        req.on('data', (buf) => {
+            body += buf.toString();
+        });
+        req.on('end', () => callback(null, body));
+        req.on('error', (err) => callback(err))
+    } else {
+        callback(null, '{}')
+    }
+}
+
 const server = http.createServer((req, res) => {
+    let cowVoice;
     req.url = url.parse(req.url);
     req.url.query = queryString.parse(req.url.query);
 
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
+    if (req.method === 'GET') {
+        if (req.url.pathname === '/') {
+            req.on('error', err => {
+                console.log(err);
+            });
 
-    req.on('data', (buffer) => {
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
 
-    });
+            res.write(`
+            <head>
+                <title>Cowsay What?</title>
+            </head>
+            <body>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/cowsay">Cowsay</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <main>
+                    <section>
+                        <h1>Say HEY COW!</h1>
+                        <p>Ever tried to make a cow talk, well you\'re in for a treat. This project does just that using a REST state API with a fun NPM library package called cowsay.</p>
+                    </section>
+                </main>
+            </body>
+            </html>
+            `);
+            res.end();
+        }
+    }
 });
 
 
