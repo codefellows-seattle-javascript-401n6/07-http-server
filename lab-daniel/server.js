09c9817d38ffced8f777d537db75ed5d5ce66232
daniel <dpillay03@gmail.com>
2018-02-27 16:39:00 -0800

    my first path is complete and my html page is sending back the anchor tag to cosway/

--- lab-daniel/server.js
+++ lab-daniel/server.js
@@ -8,45 +8,28 @@ const cowsay = require('cowsay');
 const bodyParse = require('./lib/body-parse.js');
 const PORT = process.ENV || 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(function (req, res) {
     req.url = url.parse(req.url);
     req.url.query = querystring.parse(req.url.query);
     console.log('url: ', req.url);
 
-    if(req.method === 'GET' && req.url.query === '/') {
-        //insert callback function (res)
-    }
-
-    if(req.method === 'GET' && req.url.query === '/cowsay/') {
-        //insert callback function (res)
-    }
-    
-    
-    if(req.method === 'POST' && req.url.query === '/api/cowsay/') {
-        //insert callback function (res)
+    if (req.method === 'GET' && req.url.pathname === '/') {
+        handleGet(res);
     }
 });
 
-// function handleGet(res, res) {
-//     if (err){
-//     res.writeHead(404)
-//     write(cowsay.say) -> bad file
-//     res.end
-//     return
-// }
-    //write header(200) -> It works
-    //write(cowsay.say) -> good file {text: 'it works!'}
-    //res.end
-
-//function cowsayget(res){
-    //write head(200 ) {content type: works}
-    //res.head(cowsay.say) -> good file {yaaap: yaaap}
-    //res.end
-//}
-
-//function cowpost(res)
-// parseBody
-
+function handleGet(res) {
+    fs.readFile('index.html', (err, data) => {
+        if(err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain '});
+            res.end();
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data.toString());
+        res.end();
+    });
+}
 
 server.listen(PORT, () => {
     console.log(`Your port is on http://localhost:${PORT}`);
