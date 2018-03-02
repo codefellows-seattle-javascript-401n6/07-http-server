fce4603e5bd76d3733245117f24bca4aa5862027
ixnp <enprocopios@gmail.com>
2018-03-01 22:13:35 -0800

    added post code from steves example

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ if (req.method === 'GET' && req.url.pathname === '/'){
     handelGitCowsay(res,req, cowText);
 
 } else if (req.method === 'POST' && req.url.pathname === '/api/cowsay'){
-        handelPost(res, req);
+        bodyParse(req, (err, body) =>{
+        
+        };
     };
 // else{
 //     res.writeHead(404, {'Content-Type':'text/plain'});
@@ -49,34 +51,38 @@ function handelGit(res,req){
     });
 }
 
-function handelGitCowsay(res, req, cowText){
-fs.readFile('one.html',(err,one)=>{
-    fs.readFile('two.html',(err,two)=>{
-         res.writeHead(200,{'Content-Type':'text/html'});
-     res.write(one.toString());
-     res.write(cowText);
-     res.write(two.toString());
-     res.end();
+function handelGitCowsay(res, req, cowText) {
+
+    fs.readFile('one.html', (err, one) => {
+        fs.readFile('two.html', (err, two) => {
+            if (err) {
+                res.writeHead(401, { 'Content-Type': 'text/plain' });
+                res.write({ text: '401 it hecka borked' });
+                res.end();
+                return
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(one.toString());
+                res.write(cowText);
+                res.write(two.toString());
+                res.end();
+            }
+        })
     })
-})
-   
 }
 
-function handelGit(res,req){
-    fs.readFile('part1.html',(err,data) => {
-        console.log('here is file data',data.toString())
-        if(err){
-            res.writeHead(401,{'Content-Type':'text/plain'});
-            res.write({text:'401 it hecka borked'});
-            res.end();
-            return
-        }else
-        res.writeHead(200,{'Content-Type':'text/html'});
-         res.write(data.toString());
-         res.end();
-    });
+const bodyParse = (req, callback) => {
+    if(req.method === 'POST'|| req.method ==='PUT'){
+        let body =''
+        req.on('data', (buf) => {
+            body += buf.toString();
+        });
+        req.on('end', () => callback(null, body));
+        req.on('error', (err) => callback(err))
+    }else{
+        callback(null,'{}')
+    }
 }
-
 
  const PORT = process.ENV || 3000;
  server.listen(PORT,() =>{
